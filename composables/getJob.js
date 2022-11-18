b3d0398321acbc3d26c2089a9190210126df2c8c
Dustin Quam <quamsta@gmail.com>
2022-11-18 10:35:49 -0600

    remove error handling from composable

--- composables/getJob.js
+++ composables/getJob.js
@@ -3,13 +3,7 @@ export const getJob = async (id) => {
   const feedBase = config.public.apiBase;
   const feedUrl = feedBase + "feed/positions.json?id=" + id;
   //console.log(feedUrl);
-  const { data: feed, error } = await useFetch(feedUrl, { key: feedUrl });
-
-  if (error) {
-    throw createError({ statusCode: 404, statusMessage: "Page Not Found" });
-  }
+  const { data: feed } = await useFetch(feedUrl, { key: feedUrl });
   //console.log(feedBase + "feed/positions.json");
   return feed;
-
-
 };
