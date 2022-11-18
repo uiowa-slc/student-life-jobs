b1ad6f16088d67c5abaa3c90dfaba08449ce94ef
Dustin Quam <quamsta@gmail.com>
2022-11-18 10:04:06 -0600

    throw error when we can't find a position

--- composables/getJob.js
+++ composables/getJob.js
@@ -3,7 +3,13 @@ export const getJob = async (id) => {
   const feedBase = config.public.apiBase;
   const feedUrl = feedBase + "feed/positions.json?id=" + id;
   //console.log(feedUrl);
-  const { data: feed } = await useFetch(feedUrl, { key: feedUrl });
+  const { data: feed, error } = await useFetch(feedUrl, { key: feedUrl });
+
+  if (error) {
+    throw createError({ statusCode: 404, statusMessage: "Page Not Found" });
+  }
   //console.log(feedBase + "feed/positions.json");
   return feed;
+
+
 };
