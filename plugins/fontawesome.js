75daaea904c367b4cd2b7a5d809194e1d2137a94
Dustin Quam <quamsta@gmail.com>
2023-02-23 10:11:09 -0600

    adjust include

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,12 +1,10 @@
 import { library, config } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import {
-  faMapPin,
-  faMagnifyingGlass,
-  faUser,
-  faLocationDot,
-  faArrowUpRightFromSquare,
-} from "@fortawesome/free-solid-svg-icons";
+import { faMapPin } from "@fortawesome/free-solid-svg-icons/faMapPin";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
+import { faUser } from "@fortawesome/free-solid-svg-icons/faUser";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons/faLocationDot";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons/faArrowUpRightFromSquare";
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false;
