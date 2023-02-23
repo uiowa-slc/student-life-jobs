61e63118613e6d5c273d7334f90f048baa7c8ee6
Dustin Quam <quamsta@gmail.com>
2023-02-23 09:17:25 -0600

    limit font awesome icons

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,13 +1,26 @@
 import { library, config } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMapPin,
+  faMagnifyingGlass,
+  faUser,
+  faLocationDot,
+  faArrowUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false;
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(fas);
+library.add(
+  faMapPin,
+  faMagnifyingGlass,
+  faUser,
+  faLocationDot,
+  faArrowUpRightFromSquare
+);
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component("font-awesome-icon", FontAwesomeIcon, {});
