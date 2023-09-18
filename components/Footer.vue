25be52aba575b1ad10d12765d25fb6683033966a
Brandon Ewoldt <brandon-ewoldt@uiowa.edu>
2023-09-18 09:33:54 -0500

    removes instagram snapwidget feed

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,34 +1,7 @@
 <template>
   <div>
-    <!-- Instagram Feed -->
-    <section class="instafeed pt-5" aria-labelledby="instagramid">
-      <div class="container-xl">
-        <div class="row justify-content-center">
-          <div class="col-lg-12">
-            <h3 id="instagramid">
-              Follow us on Instagram
-              <a href="https://www.instagram.com/uistudentlife/"
-                >@uistudentlife</a
-              >
-            </h3>
-            <!-- SnapWidget -->
-            <iframe
-              src="https://snapwidget.com/embed/881820"
-              class="snapwidget-widget"
-              allowtransparency="true"
-              frameborder="0"
-              scrolling="no"
-              style="border: none; overflow: hidden; width: 100%"
-              loading="lazy"
-            ></iframe>
-          </div>
-        </div>
-      </div>
-    </section>
     <footer class="footer bg-black text-white">
-      <div
-        class="footer__container container-xl footer__container--with-social"
-      >
+      <div class="footer__container container-xl footer__container--with-social">
         <div class="row py-5">
           <div class="col-12 col-md-6 col-lg-4">
             <div class="footer__info pb-3">
@@ -44,10 +17,7 @@
               <div class="footer__summary my-3">
                 <p>
                   Student Life Jobs is a service of the
-                  <a
-                    rel="noopener"
-                    href="http://studentlife.uiowa.edu"
-                    target="_blank"
+                  <a rel="noopener" href="http://studentlife.uiowa.edu" target="_blank"
                     >Division of Student Life</a
                   >.
                 </p>
@@ -73,9 +43,7 @@
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      <i
-                        class="fab fa-facebook-square fa-2x pr-2 align-middle"
-                      ></i>
+                      <i class="fab fa-facebook-square fa-2x pr-2 align-middle"></i>
                       <span class="sr-only">Facebook</span>
                     </a>
                   </li>
@@ -86,9 +54,7 @@
                       target="_blank"
                       rel="noopener noreferrer"
                     >
-                      <i
-                        class="fab fa-twitter-square fa-2x pr-2 align-middle"
-                      ></i>
+                      <i class="fab fa-twitter-square fa-2x pr-2 align-middle"></i>
                       <span class="sr-only">Twitter</span>
                     </a>
                   </li>
@@ -106,31 +72,23 @@
                     <h2 class="sr-only">Footer Navigation</h2>
                     <ul class="list-unstyled row">
                       <li class="col-6">
-                        <NuxtLink href="/" class="text-light d-block py-2"
-                          >Find a Job</NuxtLink
-                        >
+                        <NuxtLink href="/" class="text-light d-block py-2">Find a Job</NuxtLink>
                       </li>
 
                       <li class="col-6">
-                        <NuxtLink
-                          href="/working-on-campus/"
-                          class="text-light d-block py-2"
+                        <NuxtLink href="/working-on-campus/" class="text-light d-block py-2"
                           >Working on Campus
                         </NuxtLink>
                       </li>
 
                       <li class="col-6">
-                        <NuxtLink
-                          href="/departments/"
-                          class="text-light d-block py-2"
+                        <NuxtLink href="/departments/" class="text-light d-block py-2"
                           >Departments</NuxtLink
                         >
                       </li>
 
                       <li class="col-6">
-                        <NuxtLink
-                          href="/contact"
-                          class="text-light d-block py-2"
+                        <NuxtLink href="/contact" class="text-light d-block py-2"
                           >Contact Us</NuxtLink
                         >
                       </li>
@@ -155,9 +113,8 @@
           <div class="row">
             <div class="footer__copyright py-2 col">
               <p class="mb-0 small">
-                &copy; {{ currentYear }}
-                <a href="http://www.uiowa.edu/">The University of Iowa</a>. All
-                Rights Reserved.
+                &copy; {{ currentYear }} <a href="http://www.uiowa.edu/">The University of Iowa</a>.
+                All Rights Reserved.
                 <a
                   href="http://www.uiowa.edu/homepage/online-privacy-information"
                   class="footer__bar-link"
@@ -170,23 +127,14 @@
                 >
                   Nondiscrimination Statement</a
                 >
-                <a
-                  href="https://uiowa.edu/accessibility"
-                  target="_blank"
-                  class="footer__bar-link"
-                >
+                <a href="https://uiowa.edu/accessibility" target="_blank" class="footer__bar-link">
                   Accessibility</a
                 >
-                <a
-                  href="https://nativeamericancouncil.org.uiowa.edu/"
-                  class="footer__bar-link"
-                >
+                <a href="https://nativeamericancouncil.org.uiowa.edu/" class="footer__bar-link">
                   UI Indigenous Land Acknowledgement</a
                 >
                 Created by
-                <a href="https://slc.studentlife.uiowa.edu/"
-                  >Student Life Communications</a
-                >
+                <a href="https://slc.studentlife.uiowa.edu/">Student Life Communications</a>
               </p>
             </div>
           </div>
